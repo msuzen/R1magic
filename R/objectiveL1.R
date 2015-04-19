27f3f513bec05ca1f8c89f8c2addb18d57dc7278
Mehmet Suzen <msuzen@gmail.com>
2015-04-19 08:48:16 +0100

    Complex handling with objectiveL1 and objectiveL2m Lnorm.R added and DESCRIPTION update and buildPackage.bash updated

--- R/objectiveL1.R
+++ R/objectiveL1.R
@@ -1,7 +1,18 @@
+#' 
+#' Objective function for ridge L1 penalty
+#'
+#'@author Mehmet Suzen
+#'@param x, unknown vector
+#'@param T, transform bases
+#'@param phi, measurement vector
+#'@param lambda, penalty term
+#'@note Thank you Jason Xu of Washington University for handling complex numbers
+#'
 objectiveL1 <- function(x,T,phi,y,lambda) {
   # Part of R1Magic by mehmet.suzen@physics.org
   X  <- T %*% x
-  OF <- norm( phi %*% X - y , c("F") ) ^ 2 + lambda * norm( X, c("1"))
+  # OF <- norm( phi %*% X - y , c("F") ) ^ 2 + lambda * norm( X, c("1"))
+  OF <- sum(Mod(phi %*%  X - y)^2) + lambda * sum(Mod(x))
  return (OF)
 }
 
--- R/objectiveL2.R
+++ R/objectiveL2.R
@@ -1,7 +1,16 @@
-objectiveL2 <-
-function(x,T,phi,y,lambda) {
+#' 
+#' Objective function for Tikhinov L2 penalty
+#'
+#'@author Mehmet Suzen
+#'@param x, unknown vector
+#'@param T, transform bases
+#'@param phi, measurement vector
+#'@param lambda, penalty term
+#'@note Thank you Jason Xu of Washington University for complex number handling
+#'
+objectiveL2 <- function(x,T,phi,y,lambda) {
   X  <- T %*% x
-  OF <- norm( phi %*% X - y , c("F") ) ^ 2 + lambda * norm( X, c("F")) ^ 2
+  # OF <- norm( phi %*% X - y , c("F") ) ^ 2 + lambda * norm( X, c("F")) ^ 2
+  OF <-  sum(Mod(phi %*%  X - y)^2) + lambda * sum(Mod(x))^(0.5)
  return (OF)
 }
-
